1548139c0929ea63bb0961670873fca3ad945ad6
Matt Howlett <matt@mhowlett.com>
2015-04-01 07:47:55 +0700

    Added binary reader/writer extension methods

---
+++ NanomsgRPC.API/Utils/BinaryReaderExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace NanomsgRPC.API.Utils
+{
+    public static class BinaryReaderExtentions
+    {
+        public static string ReadNullableString(this BinaryReader reader)
+        {
+            byte isNull = reader.ReadByte();
+            if (isNull == 1)
+            {
+                return null;
+            }
+            return reader.ReadString();
+        }
+    }
+}
---
+++ NanomsgRPC.API/Utils/BinaryWriterExtentions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace NanomsgRPC.API.Utils
+{
+    public static class BinaryWriterExtentions
+    {
+        public static void WriteNullableString(this BinaryWriter writer, string s)
+        {
+            if (s == null)
+            {
+                writer.Write((byte) 1);
+                return;
+            }
+            writer.Write((byte) 0);
+            writer.Write(s);
+        }
+    }
+}
